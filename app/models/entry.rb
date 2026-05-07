5f568efb4698c0b3b25fc369fae00540fdd5f30f
x19370000-commits <ex19370000@gmail.com>
2026-05-07 23:51:56 +0900

    モデル間の関連付けと抽選結果のenum設定を追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,9 @@
 class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :shop
+
+  # 抽選結果の定義（0: 落選, 1: 当選）
+  enum result: { lose: 0, win: 1 }
+  # 景品交換ステータス（0: 未交換, 1: 交換済み）
+  enum status: { unexchanged: 0, exchanged: 1 }
 end
