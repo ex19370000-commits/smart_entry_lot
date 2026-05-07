f21e8411af2e0661420a3fc7e602728529ef8855
x19370000-commits <ex19370000@gmail.com>
2026-05-07 23:53:32 +0900

    Usersテーブルにnameカラムを追加し、モデルの関連付けとenum設定を完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_05_231140) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_07_145237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_05_231140) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
