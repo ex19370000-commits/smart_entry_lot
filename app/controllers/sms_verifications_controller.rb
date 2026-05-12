1e31c4b9c46404fa2315548b3af56e5b785187fa
x19370000-commits <ex19370000@gmail.com>
2026-05-12 16:18:38 +0900

    Twilio連携によるSMS認証機能を実装

---
+++ app/controllers/sms_verifications_controller.rb
@@ -0,0 +1,62 @@
+class SmsVerificationsController < ApplicationController
+  before_action :require_line_user
+
+  def new
+    @event_token = params[:event_token]
+  end
+
+  def create
+    phone_number = params[:phone_number]
+    @event_token = params[:event_token]
+
+    if phone_number.blank?
+      flash.now[:alert] = "電話番号を入力してください"
+      return render :new
+    end
+
+    code = SmsVerificationService.generate_code
+
+    begin
+      SmsVerificationService.send_code(phone_number, code)
+      current_user.update!(
+        phone_number: phone_number,
+        sms_code: code,
+        sms_code_sent_at: Time.current
+      )
+      redirect_to verify_sms_verifications_path(event_token: @event_token)
+    rescue Twilio::REST::RestError => e
+      Rails.logger.error("[Twilio Error] #{e.message}")
+      flash.now[:alert] = "SMS送信に失敗しました。電話番号をご確認ください。"
+      render :new
+    end
+  end
+
+  def verify
+    @event_token = params[:event_token]
+  end
+
+  def confirm
+    @event_token = params[:event_token]
+
+    unless SmsVerificationService.valid_code?(current_user, params[:code])
+      flash.now[:alert] = "認証コードが正しくないか、有効期限が切れています"
+      return render :verify
+    end
+
+    current_user.update!(phone_verified: true, sms_code: nil, sms_code_sent_at: nil)
+
+    if @event_token.present?
+      redirect_to event_path(@event_token), notice: "SMS認証が完了しました"
+    else
+      redirect_to root_path, notice: "SMS認証が完了しました"
+    end
+  end
+
+  private
+
+  def require_line_user
+    unless current_user&.line_user_id.present?
+      redirect_to root_path, alert: "LINEログインが必要です"
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_11_133446) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_12_140000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,8 +82,13 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_11_133446) do
     t.boolean "admin", default: false, null: false
     t.string "line_user_id"
     t.string "picture_url"
+    t.string "phone_number"
+    t.boolean "phone_verified", default: false, null: false
+    t.string "sms_code"
+    t.datetime "sms_code_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["line_user_id"], name: "index_users_on_line_user_id", unique: true
+    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
